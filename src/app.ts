4e7ae33a5dbb69088d85682abdf3b9d8c2f5be35
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-10-18 21:53:09 +0200

    feat: add OpenAPI and Body Parser

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+const yamlFilePath = path.resolve(__dirname, '../documentation/openapi.yaml');
+const swaggerDocument = YAML.load(yamlFilePath);
 
 const app: Application = express();
 
+app.use(bodyParser.json());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello, World!' });
