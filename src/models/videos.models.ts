9f76de4aa45dde295b45922f9c8f905beed75046
Nightwing3140 <pascal.fernandez352@orange.fr>
2023-10-18 20:30:19 +0200

    add <VIdeoModel>

--- src/models/videos.models.ts
+++ src/models/videos.models.ts
@@ -11,3 +11,41 @@ interface VideoModel extends Document {
   img: Buffer;
   url: string;
 }
+const videoSchema = new Schema<VideoModel>({
+    title: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    like: {
+      type: Number,
+      default: 0,
+    },
+    dislike: {
+      type: Number,
+      default: 0,
+    },
+    description: String,
+    language: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: Date,
+      required: true,
+    },
+    img: {
+      type: Buffer,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  });
+  const Video = mongoose.model<VideoModel>('Video', videoSchema);
+  
+  export default Video;
