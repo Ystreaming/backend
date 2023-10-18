d6b3bd74e69adb64c4c237eeeaa5884c620b9aba
Nightwing3140 <pascal.fernandez352@orange.fr>
2023-10-18 20:07:33 +0200

    add Video init

--- src/models/videos.models.ts
+++ src/models/videos.models.ts
@@ -15,3 +15,55 @@ class Video extends Model {
   public img!: Buffer;
   public url!: string;
 }
+Video.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      like: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      dislike: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      description: {
+        type: DataTypes.STRING, // Utilisez "STRING" pour VARCHAR
+        allowNull: true,
+      },
+      language: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      time: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      img: {
+        type: DataTypes.BLOB, // Utilisez "BLOB" pour les données binaires (image)
+        allowNull: false,
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Video',
+    }
+  );
+  
+  export { Video };
+  
