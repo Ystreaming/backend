e5795253f451ef6ad2da24418bd53ad8b27fc4fa
Nightwing3140 <pascal.fernandez352@orange.fr>
2023-10-18 19:37:41 +0200

    fix : create interface + schema

--- src/models/users.models.ts
+++ src/models/users.models.ts
@@ -3,4 +3,11 @@ interface User extends Document {
     firstName: string;
     lastName: string;
     email: string;
+    dateOfBirth: Date;
+    username: string;
+    password: string;
+    createdAt: Date;
+    status: boolean;
+    language: string;
+    profileImage: string;
 }
