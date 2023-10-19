7344e06e088e988e6c391c59aadc54454e5205c3
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-10-19 11:19:46 +0200

    feat: add road to notification

--- src/routes/notifications.route.ts
+++ src/routes/notifications.route.ts
@@ -6,8 +6,20 @@ router.get('/', (req: Request, res: Response) => {
     console.log('GET /notifications');
 });
 
+router.get('/users/:id', (req: Request, res: Response) => {
+    console.log('GET /users/:id');
+});
+
 router.post('/', (req: Request, res: Response) => {
     console.log('POST /notifications');
+});
+
+router.put('/:id', (req: Request, res: Response) => {
+    console.log('PUT /notifications/:id');
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    console.log('DELETE /notifications/:id');
 });
 
 module.exports = router;
