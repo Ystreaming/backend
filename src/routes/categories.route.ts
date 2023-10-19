3b4a325129e982c4f2e0f889995a6668217df09c
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-10-19 11:20:35 +0200

    feat: add road to categories

--- src/routes/categories.route.ts
+++ src/routes/categories.route.ts
@@ -6,8 +6,20 @@ router.get('/', (req: Request, res: Response) => {
     console.log('GET /categories');
 });
 
+router.get('/:id', (req: Request, res: Response) => {
+    console.log('GET /categories/id');
+});
+
 router.post('/', (req: Request, res: Response) => {
     console.log('POST /categories');
+});
+
+router.put('/:id', (req: Request, res: Response) => {
+    console.log('PUT /categories/id');
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    console.log('DELETE /categories/id');
 });
 
 module.exports = router;
