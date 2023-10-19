22882f3456265778c8b9a51713c1b8945ec5a648
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-10-19 13:40:24 +0200

    feat: add channels service

---
+++ src/services/channels.services.ts
@@ -0,0 +1,47 @@
+const ChannelModel = require('../models/channel.model');
+
+function getAllChannels() {
+    return ChannelModel.ChannelModel.find();
+}
+
+function getChannelById(id: number) {
+    return ChannelModel.ChannelModel.findOne({ id: id });
+}
+
+function getChannelByName(name: string) {
+    return ChannelModel.ChannelModel.findOne({ name: name });
+}
+
+function createChannel(channel: typeof ChannelModel) {
+    const newChannel = new ChannelModel.ChannelModel({
+        name: channel.name,
+        description: channel.description,
+        image: channel.image,
+        idCategories: channel.idCategories,
+        idVideos: channel.idVideos,
+    });
+    return newChannel.save();
+}
+
+function updateChannel(id: number, channel: typeof ChannelModel) {
+    return ChannelModel.ChannelModel.findOneAndUpdate({ id: id }, {
+        name: channel.name,
+        description: channel.description,
+        image: channel.image,
+        idCategories: channel.idCategories,
+        idVideos: channel.idVideos,
+    });
+}
+
+function deleteChannel(id: number) {
+    return ChannelModel.ChannelModel.findOneAndDelete({ id: id });
+}
+
+module.exports = {
+    getAllChannels,
+    getChannelById,
+    getChannelByName,
+    createChannel,
+    updateChannel,
+    deleteChannel,
+};
