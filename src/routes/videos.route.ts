078fdbb0aca23f0a0eb6b1b70d45ff8baf729731
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-10-19 11:19:05 +0200

    feat: add road videos

--- src/routes/videos.route.ts
+++ src/routes/videos.route.ts
@@ -6,8 +6,24 @@ router.get('/', (req: Request, res: Response) => {
     console.log('GET /videos');
 });
 
+router.get('/:id', (req: Request, res: Response) => {
+    console.log('GET /videos/:id');
+});
+
+router.get('/channel/:id', (req: Request, res: Response) => {
+    console.log('GET /videos/channel/:id');
+});
+
 router.post('/', (req: Request, res: Response) => {
     console.log('POST /videos');
+});
+
+router.put('/:id', (req: Request, res: Response) => {
+    console.log('PUT /videos/:id');
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    console.log('DELETE /videos/:id');
 });
 
 module.exports = router;
