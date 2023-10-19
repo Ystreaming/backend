99fbd599aa728a18ad4ef359769f4d8cd6de21c7
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-10-19 11:20:09 +0200

    feat: add road to comments

--- src/routes/comments.route.ts
+++ src/routes/comments.route.ts
@@ -6,8 +6,20 @@ router.get('/', (req: Request, res: Response) => {
     console.log('GET /comments');
 });
 
+router.get('/:id', (req: Request, res: Response) => {
+    console.log('GET /comments/:id');
+});
+
 router.post('/', (req: Request, res: Response) => {
     console.log('POST /comments');
+});
+
+router.put('/:id', (req: Request, res: Response) => {
+    console.log('PUT /comments/:id');
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    console.log('DELETE /comments/:id');
 });
 
 module.exports = router;
