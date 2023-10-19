8b83df2183598c9aba7298f71fc8973652b698fa
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-10-19 11:20:20 +0200

    feat: add road to channels

--- src/routes/channels.route.ts
+++ src/routes/channels.route.ts
@@ -6,8 +6,24 @@ router.get('/', (req: Request, res: Response) => {
     console.log('GET /channels');
 });
 
+router.get('/:id', (req: Request, res: Response) => {
+    console.log('GET /channels/:id');
+});
+
+router.get('/:name', (req: Request, res: Response) => {
+    console.log('GET /channels/:name');
+});
+
 router.post('/', (req: Request, res: Response) => {
     console.log('POST /channels');
+});
+
+router.put('/:id', (req: Request, res: Response) => {
+    console.log('PUT /channels/:id');
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    console.log('DELETE /channels/:id');
 });
 
 module.exports = router;
