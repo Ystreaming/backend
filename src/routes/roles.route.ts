382b1ed8a3eb8a4c651121ffe4294f9673dbc6fb
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-10-19 11:19:20 +0200

    feat: add road roles

--- src/routes/roles.route.ts
+++ src/routes/roles.route.ts
@@ -6,8 +6,20 @@ router.get('/', (req: Request, res: Response) => {
     console.log('GET /roles');
 });
 
+router.get('/:id', (req: Request, res: Response) => {
+    console.log('GET /roles/:id');
+});
+
 router.post('/', (req: Request, res: Response) => {
     console.log('POST /roles');
+});
+
+router.put('/:id', (req: Request, res: Response) => {
+    console.log('PUT /roles/:id');
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    console.log('DELETE /roles/:id');
 });
 
 module.exports = router;
