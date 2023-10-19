d871118dfda92e96cf8ed70d19c36a2fca9feea8
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-10-19 11:20:50 +0200

    feat: add road to users

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -6,8 +6,24 @@ router.get('/', (req: Request, res: Response) => {
     console.log('GET /users');
 });
 
+router.get('/:id', (req: Request, res: Response) => {
+    console.log('GET /users/:id');
+});
+
+router.get('/username/:username:', (req: Request, res: Response) => {
+    console.log('GET /users/username/:username');
+});
+
 router.post('/', (req: Request, res: Response) => {
     console.log('POST /users');
+});
+
+router.put('/:id', (req: Request, res: Response) => {
+    console.log('PUT /users/:id');
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    console.log('DELETE /users/:id');
 });
 
 module.exports = router;
