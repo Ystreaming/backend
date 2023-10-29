48b10e99e3cb313d0e2ece00af87313a4014950e
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-10-29 11:05:43 +0100

    refactor: Update notification route

--- src/routes/notifications.route.ts
+++ src/routes/notifications.route.ts
@@ -2,16 +2,20 @@ import { Request, Response } from 'express';
 const express = require('express');
 const router = express.Router();
 
+// => /Notification
+
 router.get('/', (req: Request, res: Response) => {
     console.log('GET /notifications');
 });
 
-router.get('/users/:id', (req: Request, res: Response) => {
-    console.log('GET /users/:id');
-});
-
 router.post('/', (req: Request, res: Response) => {
     console.log('POST /notifications');
+});
+
+// => /Notification/id
+
+router.get('/:id', (req: Request, res: Response) => {
+    console.log('GET /notifications/:id');
 });
 
 router.put('/:id', (req: Request, res: Response) => {
@@ -20,6 +24,12 @@ router.put('/:id', (req: Request, res: Response) => {
 
 router.delete('/:id', (req: Request, res: Response) => {
     console.log('DELETE /notifications/:id');
+});
+
+// => /Notification/user/id
+
+router.get('/users/:id', (req: Request, res: Response) => {
+    console.log('GET /users/:id');
 });
 
 module.exports = router;
