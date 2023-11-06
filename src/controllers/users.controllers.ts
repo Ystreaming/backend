3f65c6a61f9bacf4b8ccb5479369e50f7d003d07
Nightwing3140 <pascal.fernandez352@orange.fr>
2023-11-06 10:01:37 +0100

    add loginUser

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -18,9 +18,7 @@ async function getAllUsers(req: Request, res: Response) {
   async function getUserById(req: Request, res: Response) {
     if (!Number.isInteger(parseInt(req.params.id))) {
         return res.status(400).json({ message: 'Id must be an integer' });
-    } else if (req.params.id < 0) {
-        return res.status(400).json({ message: 'Id must be a positive integer' });
-    } else {
+    } else  {
         const user = await UsersService.getUserById(req.params.id);
 
         if (!user) {
@@ -30,41 +28,22 @@ async function getAllUsers(req: Request, res: Response) {
         }
     }
 }
-    async function loginUser(req: Request, res: Response) {
-        if (!req.body.email || !req.body.password) {
-            return res.status(400).json({ message: 'email and password are required' });
-        } else {
-            const user = await UsersService.loginUser(req.body.email, req.body.password);
-            if (!user) {
-                return res.status(404).json({ message: 'User not found' });
-            } else {
-                const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-                res.cookie('token', token, { httpOnly: true });
-                return res.status(200).json({ message: 'User logged in' });
-            }
-    }
-}
-async function updateUser(req: Request, res: Response) {
-    if (!Number.isInteger(parseInt(req.params.id))) {
-        return res.status(400).json({ message: 'Id must be an integer' });
-    } else if (req.params.id < 0) {
-        return res.status(400).json({ message: 'Id must be a positive integer' });
-    } else if (!req.body.firstName || !req.body.firstName || !req.body.email || !req.body.password) {
-        return res.status(400).json({ message: 'firstName, lastName, email and password are required' });
+async function loginUser(req: Request, res: Response) {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({ message: 'email and password are required' });
     } else {
-        const user = await UsersService.updateUser(req.params.id, req.body);
-
+        const user = await UsersService.loginUser(req.body.email, req.body.password);
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         } else {
-            return res.status(200).json(user);
+            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res.cookie('token', token, { httpOnly: true });
+            return res.status(200).json({ message: 'User logged in' });
         }
     }
 }
-
   module.exports = {
     getAllUsers,
     getUserById,
-    loginUser,
-
+    loginUser
 };
