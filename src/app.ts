8a90f63e2d2aa51a3be39777fe1b7ee3f00488fc
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-11-07 21:03:11 +0100

    feat: upload file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import express, { Application, Request, Response } from 'express';
-import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
 const yamlFilePath = path.resolve(__dirname, '../documentation/openapi.yaml');
 const swaggerDocument = YAML.load(yamlFilePath);
-const OpenApiValidator = require('express-openapi-validator');
 
 const app: Application = express();
 
@@ -16,30 +14,23 @@ const VideosRoute = require('./routes/videos.route');
 const ChannelsRoute = require('./routes/channels.route');
 const CommentsRoute = require('./routes/comments.route');
 const HistoricsRoute = require('./routes/historics.route');
+const FileRoute = require('./routes/file.route');
 
-
-app.use(bodyParser.json());
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use(
-  OpenApiValidator.middleware({
-    apiSpec: './openapi.yaml',
-    validateRequests: true,
-  }),
-);
-
 app.use('/users', UsersRoute);
 app.use('/roles', RolesRoute);
-app.use('/Category', CategoryRoute);
+app.use('/category', CategoryRoute);
 app.use('/videos', VideosRoute);
 app.use('/channels', ChannelsRoute);
 app.use('/comments', CommentsRoute);
 app.use('/historics', HistoricsRoute);
+app.use('/files', FileRoute);
 
 app.use((err: Error, req: Request, res: Response, next: Function) => {
   res.status(500).json({
-    message: err.message
+    message: err.message,
   });
 });
 
