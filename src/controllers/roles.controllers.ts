e5f014dfd5837caa0ace9e9cd30f5e9920650491
Nightwing3140 <pascal.fernandez352@orange.fr>
2023-11-07 09:45:32 +0100

    feat(controllers-page): create page notifications

--- src/controllers/roles.controllers.ts
+++ src/controllers/roles.controllers.ts
@@ -1,44 +1,44 @@
 import { Request, Response } from 'express';
 const RolesService = require('../services/roles.service');
 
-async function getAllRoles(req: Request, res: Response) {
-    try {
-      const roles = await RolesService.getAllRoles();
-      if (!roles) {
-        res.status(404).json({ message: 'Role not found' });
-      } else {
-        res.status(200).json(roles);
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-}
-async function updateRoles(req: Request, res: Response) {
-    if (!Number.isInteger(parseInt(req.params.id))) {
-        return res.status(400).json({ message: 'Id must be an integer' });
-    } else {
-        const roles = await RolesService.updateRoles(req.params.id, req.body);
-
+    async function getAllRoles(req: Request, res: Response) {
+        try {
+        const roles = await RolesService.getAllRoles();
         if (!roles) {
-            return res.status(404).json({ message: 'Role not found' });
+            res.status(404).json({ message: 'Role not found' });
         } else {
-            return res.status(200).json(roles);
+            res.status(200).json(roles);
         }
-    }
+        } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+        }
 }
-async function deleteRoles(req: Request, res: Response) {
-    if (!Number.isInteger(parseInt(req.params.id))) {
-        return res.status(400).json({ message: 'Id must be an integer' });
-    } else {
-        const roles = await RolesService.deleteUser(req.params.id);
-
-        if (!roles) {
-            return res.status(404).json({ message: 'Role not found' });
+    async function updateRoles(req: Request, res: Response) {
+        if (!Number.isInteger(parseInt(req.params.id))) {
+            return res.status(400).json({ message: 'Id must be an integer' });
         } else {
-            return res.status(200).json({ message: 'Role deleted' });
+            const roles = await RolesService.updateRoles(req.params.id, req.body);
+
+            if (!roles) {
+                return res.status(404).json({ message: 'Role not found' });
+            } else {
+                return res.status(200).json(roles);
+            }
         }
-    }
+}
+    async function deleteRoles(req: Request, res: Response) {
+        if (!Number.isInteger(parseInt(req.params.id))) {
+            return res.status(400).json({ message: 'Id must be an integer' });
+        } else {
+            const roles = await RolesService.deleteUser(req.params.id);
+
+            if (!roles) {
+                return res.status(404).json({ message: 'Role not found' });
+            } else {
+                return res.status(200).json({ message: 'Role deleted' });
+            }
+        }
 }
   module.exports = {
     getAllRoles,
--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -2,20 +2,20 @@ import { Request, Response } from 'express';
 const UsersService = require('../services/users.service');
 const jwt = require('jsonwebtoken');
 
-async function getAllUsers(req: Request, res: Response) {
-    try {
-      const users = await UsersService.getAllUsers();
-      if (!users) {
-        res.status(404).json({ message: 'Users not found' });
-      } else {
-        res.status(200).json(users);
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
+    async function getAllUsers(req: Request, res: Response) {
+        try {
+        const users = await UsersService.getAllUsers();
+        if (!users) {
+            res.status(404).json({ message: 'Users not found' });
+        } else {
+            res.status(200).json(users);
+        }
+        } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
     }
 }
-  async function getUserById(req: Request, res: Response) {
+    async function getUserById(req: Request, res: Response) {
     if (!Number.isInteger(parseInt(req.params.id))) {
         return res.status(400).json({ message: 'Id must be an integer' });
     } else  {
@@ -28,47 +28,47 @@ async function getAllUsers(req: Request, res: Response) {
         }
     }
 }
-async function loginUser(req: Request, res: Response) {
-    if (!req.body.email || !req.body.password) {
-        return res.status(400).json({ message: 'email and password are required' });
-    } else {
-        const user = await UsersService.loginUser(req.body.email, req.body.password);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+    async function loginUser(req: Request, res: Response) {
+        if (!req.body.email || !req.body.password) {
+            return res.status(400).json({ message: 'email and password are required' });
         } else {
-            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-            res.cookie('token', token, { httpOnly: true });
-            return res.status(200).json({ message: 'User logged in' });
+            const user = await UsersService.loginUser(req.body.email, req.body.password);
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            } else {
+                const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+                res.cookie('token', token, { httpOnly: true });
+                return res.status(200).json({ message: 'User logged in' });
+            }
         }
-    }
 }
-async function updateUser(req: Request, res: Response) {
-    if (!Number.isInteger(parseInt(req.params.id))) {
-        return res.status(400).json({ message: 'Id must be an integer' });
-    } else if (!req.body.firstName || !req.body.firstName || !req.body.email || !req.body.password) {
-        return res.status(400).json({ message: 'firstName, lastName, email and password are required' });
-    } else {
-        const user = await UsersService.updateUser(req.params.id, req.body);
-
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+    async function updateUser(req: Request, res: Response) {
+        if (!Number.isInteger(parseInt(req.params.id))) {
+            return res.status(400).json({ message: 'Id must be an integer' });
+        } else if (!req.body.firstName || !req.body.firstName || !req.body.email || !req.body.password) {
+            return res.status(400).json({ message: 'firstName, lastName, email and password are required' });
         } else {
-            return res.status(200).json(user);
+            const user = await UsersService.updateUser(req.params.id, req.body);
+
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            } else {
+                return res.status(200).json(user);
+            }
         }
-    }
 }
-async function deleteUser(req: Request, res: Response) {
-    if (!Number.isInteger(parseInt(req.params.id))) {
-        return res.status(400).json({ message: 'Id must be an integer' });
-    } else {
-        const user = await UsersService.deleteUser(req.params.id);
-
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+    async function deleteUser(req: Request, res: Response) {
+        if (!Number.isInteger(parseInt(req.params.id))) {
+            return res.status(400).json({ message: 'Id must be an integer' });
         } else {
-            return res.status(200).json({ message: 'User deleted' });
+            const user = await UsersService.deleteUser(req.params.id);
+
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            } else {
+                return res.status(200).json({ message: 'User deleted' });
+            }
         }
-    }
 }
   module.exports = {
     getAllUsers,
