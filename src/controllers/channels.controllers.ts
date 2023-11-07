6780dde80f71e2c71419ca5ba42fc19c48ac7dde
Nightwing3140 <pascal.fernandez352@orange.fr>
2023-11-07 14:12:28 +0100

    add new pages controllers

---
+++ src/controllers/channels.controllers.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+const ChannelService = require('../services/channels.service');
+
+    async function getAllChannel(req: Request, res: Response) {
+        try {
+        const channel = await ChannelService.getAllChannel();
+        if (!channel) {
+            res.status(404).json({ message: 'Channel not found' });
+        } else {
+            res.status(200).json(channel);
+        }
+        } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+        }
+}
+    async function addChannel(req: Request, res: Response) {
+        if (!req.body.idUser || !req.body.idVideo || !req.body.title || !req.body.type) {
+            return res.status(400).json({ message: 'idUser, idVideo, title and type are required' });
+        } else {
+            const channel = await ChannelService.addChannel(req.body);
+
+            if (!channel) {
+                return res.status(400).json({ message: 'Channel not created' });
+            } else {
+                return res.status(201).json(channel);
+            }
+        }
+}
+    async function updateChannel(req: Request, res: Response) {
+        if (!Number.isInteger(parseInt(req.params.id))) {
+            return res.status(400).json({ message: 'Id must be an integer' });
+        } else {
+            const channel = await ChannelService.updateChannel(req.params.id, req.body);
+
+            if (!channel) {
+                return res.status(404).json({ message: 'Channel not found' });
+            } else {
+                return res.status(200).json(channel);
+            }
+        }
+}
+    async function deleteChannel(req: Request, res: Response) {
+        if (!Number.isInteger(parseInt(req.params.id))) {
+            return res.status(400).json({ message: 'Id must be an integer' });
+        } else {
+            const channel = await ChannelService.deleteChannel(req.params.id);
+
+            if (!channel) {
+                return res.status(404).json({ message: 'Channel not found' });
+            } else {
+                return res.status(200).json({ message: 'Channel deleted' });
+            }
+        }
+}
+  module.exports = {
+    getAllChannel,
+    addChannel,
+    updateChannel,
+    deleteChannel
+};
