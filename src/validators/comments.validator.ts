b33733972be100dc244bf639d7f41797e659d14c
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-11-09 10:51:06 +0100

    add comments validator
    Co-authored-by: Nightwing3140 <vincent.fernandezpro@gmail.com>

---
+++ src/validators/comments.validator.ts
@@ -0,0 +1,31 @@
+import { body } from 'express-validator';
+
+export const videoValidator = [
+
+    body('texte')
+        .isString()
+        .isLength({ min: 3 }).withMessage('Texte should be at least 5 characters')
+        .exists()
+        .notEmpty().withMessage('Texte is required'),
+
+    body('like')
+        .isNumeric()
+        .exists()
+        .notEmpty().withMessage('Like is required'),
+
+    body('dislike')
+        .isNumeric()
+        .exists()
+        .notEmpty().withMessage('Dislike is required'),
+
+    body('createdAt')
+        .isDate().withMessage('Invalid date')
+        .exists()
+        .notEmpty().withMessage('Date of birth is required'),
+        
+    body('idUser')
+        .isString()
+        .isLength({ min: 3 }).withMessage('IdUser should be at least 5 characters')
+        .exists()
+        .notEmpty().withMessage('IdUser is required'),
+];
