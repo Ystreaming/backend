0ade98b95c797a6bb06480c33b799422f35d2591
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-11-09 10:49:43 +0100

    add channels validator
    Co-authored-by: Nightwing3140 <vincent.fernandezpro@gmail.com>

---
+++ src/validators/channels.validator.ts
@@ -0,0 +1,16 @@
+import { body } from 'express-validator';
+
+export const categoryValidator = [
+
+    body('name')
+        .isString()
+        .isLength({ min: 3 }).withMessage('Name should be at least 5 characters')
+        .exists()
+        .notEmpty().withMessage('Name is required'),
+
+    body('description')
+        .isString()
+        .isLength({ min: 3 }).withMessage('Description should be at least 5 characters')
+        .exists()
+        .notEmpty().withMessage('Description is required'),
+];
