506b35fd225dc9f9007912f9385008c4c79f8035
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-11-28 16:07:40 +0100

    feat: add upload image on all route

--- src/controllers/categories.controllers.ts
+++ src/controllers/categories.controllers.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 const CategoryService = require('../services/categories.services');
+const fileService = require('../services/files.services');
 
-    async function getAllCategory(req: Request, res: Response) {
-        try {
+async function getAllCategory(req: Request, res: Response) {
+    try {
         const category = await CategoryService.getAllCategory();
         if (!category) {
             res.status(204).json({ message: 'Category not found' });
@@ -11,62 +12,74 @@ const CategoryService = require('../services/categories.services');
             res.status(200).json(category);
         }
         } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-        }
+            console.error(error);
+            res.status(500).json({ message: 'Internal Server Error' });
+    }
 }
-    async function createCategory(req: Request, res: Response) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ error: 'Validation failed', details: errors.array() });
+
+async function createCategory(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: 'Validation failed', details: errors.array() });
+    }
+
+    try {
+        let categoryData = req.body;
+
+        if (req.file) {
+            const file = await fileService.createFile(req.file);
+            categoryData.image = file._id;
         }
 
-        try {
-            const newCategory = await CategoryService.createCategory(req.body);
-            return res.status(201).json(newCategory);
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ message: 'Internal Server Error' });
+        const newCategory = await CategoryService.createCategory(categoryData);
+        return res.status(201).json(newCategory);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+
+async function getCategoryById(req: Request, res: Response) {
+    if (!Number.isInteger(parseInt(req.params.id))) {
+        return res.status(400).json({ message: 'Id must be an integer' });
+    } else  {
+        const category = await CategoryService.getCategoryById(req.params.id);
+
+        if (!category) {
+            return res.status(404).json({ message: 'Category not found' });
+        } else {
+            return res.status(200).json(category);
         }
     }
-    async function getCategoryById(req: Request, res: Response) {
-        if (!Number.isInteger(parseInt(req.params.id))) {
-            return res.status(400).json({ message: 'Id must be an integer' });
-        } else  {
-            const category = await CategoryService.getCategoryById(req.params.id);
-
-            if (!category) {
-                return res.status(404).json({ message: 'Category not found' });
-            } else {
-                return res.status(200).json(category);
-            }
-        }
 }
-    async function updateCategory(req: Request, res: Response) {
-        if (!Number.isInteger(parseInt(req.params.id))) {
-            return res.status(400).json({ message: 'Id must be an integer' });
-        } else {
-            const category = await CategoryService.updateCategory(req.params.id, req.body);
 
-            if (!category) {
-                return res.status(404).json({ message: 'Category not found' });
-            } else {
-                return res.status(200).json(category);
-            }
+async function updateCategory(req: Request, res: Response) {
+    if (!Number.isInteger(parseInt(req.params.id))) {
+        return res.status(400).json({ message: 'Id must be an integer' });
+    } else {
+        const category = await CategoryService.updateCategory(req.params.id, req.body);
+
+        if (!category) {
+            return res.status(404).json({ message: 'Category not found' });
+        } else {
+            return res.status(200).json(category);
         }
+    }
 }
-    async function deleteCategory(req: Request, res: Response) {
-        if (!Number.isInteger(parseInt(req.params.id))) {
-            return res.status(400).json({ message: 'Id must be an integer' });
-        } else {
-            const category = await CategoryService.deleteCategory(req.params.id);
 
-            if (!category) {
-                return res.status(404).json({ message: 'Category not found' });
-            } else {
-                return res.status(200).json({ message: 'Category deleted' });
-            }
+async function deleteCategory(req: Request, res: Response) {
+    if (!Number.isInteger(parseInt(req.params.id))) {
+        return res.status(400).json({ message: 'Id must be an integer' });
+    } else {
+        const category = await CategoryService.deleteCategory(req.params.id);
+
+        if (!category) {
+            return res.status(404).json({ message: 'Category not found' });
+        } else {
+            return res.status(200).json({ message: 'Category deleted' });
         }
+    }
 }
   module.exports = {
     getAllCategory,
--- src/controllers/notifications.controllers.ts
+++ src/controllers/notifications.controllers.ts
@@ -2,73 +2,78 @@ import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 const NotificationService = require('../services/notifications.services');
 
-    async function getAllNotification(req: Request, res: Response) {
-        try {
+async function getAllNotification(req: Request, res: Response) {
+    try {
         const notification = await NotificationService.getAllNotifications();
         if (!notification) {
             res.status(204).json({ message: 'Notification not found' });
         } else {
             res.status(200).json(notification);
         }
-        } catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal Server Error' });
-        }
+    }
 }
-    async function createNotification(req: Request, res: Response) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ error: 'Validation failed', details: errors.array() });
-        }
 
-        try {
-            const newNotification = await NotificationService.createNotification(req.body);
-            return res.status(201).json(newNotification);
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ message: 'Internal Server Error' });
+async function createNotification(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: 'Validation failed', details: errors.array() });
+    }
+
+    try {
+        const newNotification = await NotificationService.createNotification(req.body);
+        return res.status(201).json(newNotification);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+async function getNotificationById(req: Request, res: Response) {
+    if (!Number.isInteger(parseInt(req.params.id))) {
+        return res.status(400).json({ message: 'Id must be an integer' });
+    } else  {
+        const notification = await NotificationService.getNotificationById(req.params.id);
+
+        if (!notification) {
+            return res.status(404).json({ message: 'Notification not found' });
+        } else {
+            return res.status(200).json(notification);
         }
     }
-    async function getNotificationById(req: Request, res: Response) {
-        if (!Number.isInteger(parseInt(req.params.id))) {
-            return res.status(400).json({ message: 'Id must be an integer' });
-        } else  {
-            const notification = await NotificationService.getNotificationById(req.params.id);
+}
 
-            if (!notification) {
-                return res.status(404).json({ message: 'Notification not found' });
-            } else {
-                return res.status(200).json(notification);
-            }
+async function updateNotification(req: Request, res: Response) {
+    if (!Number.isInteger(parseInt(req.params.id))) {
+        return res.status(400).json({ message: 'Id must be an integer' });
+    } else {
+        const notification = await NotificationService.updateNotification(req.params.id, req.body);
+
+        if (!notification) {
+            return res.status(404).json({ message: 'Notification not found' });
+        } else {
+            return res.status(200).json(notification);
         }
     }
-    async function updateNotification(req: Request, res: Response) {
-        if (!Number.isInteger(parseInt(req.params.id))) {
-            return res.status(400).json({ message: 'Id must be an integer' });
-        } else {
-            const notification = await NotificationService.updateNotification(req.params.id, req.body);
-
-            if (!notification) {
-                return res.status(404).json({ message: 'Notification not found' });
-            } else {
-                return res.status(200).json(notification);
-            }
-        }
 }
-    async function deleteNotification(req: Request, res: Response) {
-        if (!Number.isInteger(parseInt(req.params.id))) {
-            return res.status(400).json({ message: 'Id must be an integer' });
-        } else {
-            const notification = await NotificationService.deleteNotification(req.params.id);
 
-            if (!notification) {
-                return res.status(404).json({ message: 'Notification not found' });
-            } else {
-                return res.status(200).json({ message: 'Notification deleted' });
-            }
+async function deleteNotification(req: Request, res: Response) {
+    if (!Number.isInteger(parseInt(req.params.id))) {
+        return res.status(400).json({ message: 'Id must be an integer' });
+    } else {
+        const notification = await NotificationService.deleteNotification(req.params.id);
+
+        if (!notification) {
+            return res.status(404).json({ message: 'Notification not found' });
+        } else {
+            return res.status(200).json({ message: 'Notification deleted' });
         }
+    }
 }
-  module.exports = {
+
+module.exports = {
     getAllNotification,
     createNotification,
     getNotificationById,
