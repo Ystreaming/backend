e3a3347184d4a934d00b388b43aa6bd0e1b60231
Nightwing3140 <pascal.fernandez352@orange.fr>
2023-11-30 10:56:54 +0100

    fix middleware video

--- src/middlewares/videosmiddleware.ts
+++ src/middlewares/videosmiddleware.ts
@@ -4,78 +4,70 @@ import fs from 'fs';
 import path from 'path';
 
 declare module 'express-serve-static-core' {
-  interface Request {
-    filesList?: { name: string; url: string }[];
-    filePath?: string;
+    interface Request {
+      filesList?: { name: string; url: string }[];
+      filePath?: string;
+    }
   }
-}
 
-const uploadsDirectory = path.join(__dirname, '../../uploads/files');
+const uploadsDirectory = path.join(__dirname, '../../uploads/video');
 
 const storage: StorageEngine = multer.diskStorage({
-  destination: (req, file, cb) => {
-    fs.mkdirSync(uploadsDirectory, { recursive: true });
-    cb(null, uploadsDirectory);
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
+    destination: (req, file, cb) => {
+        fs.mkdirSync(uploadsDirectory, { recursive: true });
+        cb(null, uploadsDirectory);
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
 });
 
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 1024 * 1024, // 1MB limit
-  },
-});
+const upload = multer({ storage });
 
-// Middleware for uploading a single video file
+// Middleware pour l'upload d'un fichier
 function uploadSingleVideo(fieldName: string) {
-  return upload.single(fieldName);
+    return upload.single(fieldName);
 }
 
-// Middleware for listing all files
-function listFiles(req: Request, res: Response, next: NextFunction) {
-  fs.readdir(uploadsDirectory, (err, files) => {
-    if (err) {
-      return res.status(500).send({ message: 'Unable to scan files!' });
+// Middleware pour lister tous les fichiers
+function listVideo(req: Request, res: Response, next: NextFunction) {
+    fs.readdir(uploadsDirectory, (err, files) => {
+        if (err) {
+            return res.status(500).send({ message: "Unable to scan files!" });
+        }
+        req.filesList = files.map(file => ({ name: file, url: `${req.protocol}://${req.get('host')}/files/${file}` }));
+        next();
+    });
+}
+
+// Middleware pour vérifier si le fichier existe
+function checkVideoExists(req: Request, res: Response, next: NextFunction) {
+    const fileName = req.params.id;
+    const filePath = path.join(uploadsDirectory, fileName);
+
+    if (!fs.existsSync(filePath)) {
+        return res.status(404).send({ message: "File not found" });
     }
-    req.filesList = files.map((file) => ({
-      name: file,
-      url: `${req.protocol}://${req.get('host')}/files/${file}`,
-    }));
+
+    req.filePath = filePath;
     next();
-  });
 }
 
-// Middleware for checking if the video file exists
-function checkVideoFileExists(req: Request, res: Response, next: NextFunction) {
-  const fileName = req.params.id;
-  const filePath = path.join(uploadsDirectory, fileName);
+// Middleware pour supprimer un fichier
+function deleteVideo(req: Request, res: Response, next: NextFunction) {
+    const filePath = req.filePath as string;
 
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).send({ message: 'Video file not found' });
-  }
-
-  req.filePath = filePath;
-  next();
-}
-
-// Middleware for deleting a video file
-function deleteVideoFile(req: Request, res: Response, next: NextFunction) {
-  const filePath = req.filePath as string;
-
-  fs.unlink(filePath, (err) => {
-    if (err) {
-      return res.status(500).send({ message: err.message });
-    }
-    next();
-  });
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            return res.status(500).send({ message: err.message });
+        }
+        next();
+    });
 }
 
 export {
-  uploadSingleVideo,
-  listFiles,
-  checkVideoFileExists,
-  deleteVideoFile,
+    uploadSingleVideo,
+    listVideo,
+    checkVideoExists,
+    deleteVideo
 };
