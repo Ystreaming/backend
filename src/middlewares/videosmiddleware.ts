c409fd7c227e978c42eece0cc6f123724cdfe98f
Nightwing3140 <pascal.fernandez352@orange.fr>
2023-11-30 09:23:59 +0100

    add middleware videos to limit

---
+++ src/middlewares/videosmiddleware.ts
@@ -0,0 +1,81 @@
+import { Request, Response, NextFunction } from 'express';
+import multer, { StorageEngine } from 'multer';
+import fs from 'fs';
+import path from 'path';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    filesList?: { name: string; url: string }[];
+    filePath?: string;
+  }
+}
+
+const uploadsDirectory = path.join(__dirname, '../../uploads/files');
+
+const storage: StorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    fs.mkdirSync(uploadsDirectory, { recursive: true });
+    cb(null, uploadsDirectory);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024 * 1024, // 1MB limit
+  },
+});
+
+// Middleware for uploading a single video file
+function uploadSingleVideo(fieldName: string) {
+  return upload.single(fieldName);
+}
+
+// Middleware for listing all files
+function listFiles(req: Request, res: Response, next: NextFunction) {
+  fs.readdir(uploadsDirectory, (err, files) => {
+    if (err) {
+      return res.status(500).send({ message: 'Unable to scan files!' });
+    }
+    req.filesList = files.map((file) => ({
+      name: file,
+      url: `${req.protocol}://${req.get('host')}/files/${file}`,
+    }));
+    next();
+  });
+}
+
+// Middleware for checking if the video file exists
+function checkVideoFileExists(req: Request, res: Response, next: NextFunction) {
+  const fileName = req.params.id;
+  const filePath = path.join(uploadsDirectory, fileName);
+
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).send({ message: 'Video file not found' });
+  }
+
+  req.filePath = filePath;
+  next();
+}
+
+// Middleware for deleting a video file
+function deleteVideoFile(req: Request, res: Response, next: NextFunction) {
+  const filePath = req.filePath as string;
+
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      return res.status(500).send({ message: err.message });
+    }
+    next();
+  });
+}
+
+export {
+  uploadSingleVideo,
+  listFiles,
+  checkVideoFileExists,
+  deleteVideoFile,
+};
